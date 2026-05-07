e044760e2d87861fa5e0648a0ef90bb2a1a59246
techwteddy <techwithteddy@gmail.com>
2026-05-07 15:00:43 -0700

    Delete dinq/dinq directory

--- dinq/dinq/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Allow the Anthropic API calls from API routes
-  serverExternalPackages: [],
-}
-
-module.exports = nextConfig
